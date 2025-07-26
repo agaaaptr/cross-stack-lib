73e3cfdffcdee507bf23b8234e3c410563609bbc
agaa <92021248+agaaaptr@users.noreply.github.com>
2025-07-26 22:55:51 +0700

    fix(docs): Explicitly configure PostCSS and Tailwind in next.config.js and remove postcss.config.js

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -3,6 +3,27 @@ const withMDX = require('@next/mdx')();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        'style-loader', // or MiniCssExtractPlugin.loader in production
+        'css-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [
+                require(require.resolve('tailwindcss'))(require.resolve('./tailwind.config.js')),
+                require(require.resolve('autoprefixer')),
+              ],
+            },
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
 
 module.exports = withMDX(nextConfig);
